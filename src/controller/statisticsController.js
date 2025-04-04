90bec963c0ee9e9abe666aae6235fefe1104f899
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2025-04-04 17:01:43 +0700

    feat: add endpoints to retrieve total views and downloads of scientific papers by author

--- src/controller/statisticsController.js
+++ src/controller/statisticsController.js
@@ -3,15 +3,131 @@ const ScientificPaper = require("../models/ScientificPaper");
 const statisticsController = {
   getTotalPapersByAuthorId: async (req, res) => {
     try {
-      const { author_id } = req.params.author_id;
+      const { author_id } = req.params;
 
-      const totalPapers = await ScientificPaper.countDocuments({
-        author: author_id,
-      });
+      const result = await ScientificPaper.aggregate([
+        {
+          $lookup: {
+            from: "paperauthors",
+            localField: "author",
+            foreignField: "_id",
+            as: "authorDetails",
+          },
+        },
+        {
+          $match: {
+            "authorDetails.user_id": author_id.toString(),
+            status: "approved",
+          },
+        },
+        {
+          $count: "totalPapers",
+        },
+      ]);
+
+      const totalPapers = result.length > 0 ? result[0].totalPapers : 0;
 
       res.status(200).json({
         author_id,
         total_papers: totalPapers,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  getTotalViewsByAuthorId: async (req, res) => {
+    try {
+      const { author_id } = req.params;
+
+      const result = await ScientificPaper.aggregate([
+        {
+          $lookup: {
+            from: "paperviews",
+            localField: "_id",
+            foreignField: "paper_id",
+            as: "viewDetails",
+          },
+        },
+        {
+          $lookup: {
+            from: "paperauthors",
+            localField: "author",
+            foreignField: "_id",
+            as: "authorDetails",
+          },
+        },
+        {
+          $match: {
+            "authorDetails.user_id": author_id.toString(),
+            status: "approved",
+          },
+        },
+        {
+          $unwind: "$viewDetails",
+        },
+        {
+          $group: {
+            _id: null,
+            totalViews: { $sum: 1 },
+          },
+        },
+      ]);
+
+      const totalViews = result.length > 0 ? result[0].totalViews : 0;
+
+      res.status(200).json({
+        author_id,
+        total_views: totalViews,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  getTotalDownloadsByAuthorId: async (req, res) => {
+    try {
+      const { author_id } = req.params;
+
+      const result = await ScientificPaper.aggregate([
+        {
+          $lookup: {
+            from: "paperdownloads",
+            localField: "_id",
+            foreignField: "paper_id", 
+            as: "downloadDetails", 
+          },
+        },
+        {
+          $lookup: {
+            from: "paperauthors", 
+            localField: "author",
+            foreignField: "_id", 
+            as: "authorDetails", 
+          },
+        },
+        {
+          $match: {
+            "authorDetails.user_id": author_id.toString(),
+            status: "approved", 
+          },
+        },
+        {
+          $unwind: "$downloadDetails",
+        },
+        {
+          $group: {
+            _id: null, 
+            totalDownloads: { $sum: 1 }, 
+          },
+        },
+      ]);
+
+      const totalDownloads = result.length > 0 ? result[0].totalDownloads : 0;
+
+      res.status(200).json({
+        author_id,
+        total_downloads: totalDownloads,
       });
     } catch (error) {
       res.status(500).json({ message: error.message });
--- src/routes/statisticsRoutes.js
+++ src/routes/statisticsRoutes.js
@@ -18,6 +18,17 @@ const statisticsController = require("../controller/statisticsController");
  *     responses:
  *       200:
  *         description: Total number of scientific papers retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 author_id:
+ *                   type: string
+ *                   description: ID of the author
+ *                 total_papers:
+ *                   type: number
+ *                   description: Total number of scientific papers
  *       500:
  *         description: Internal server error
  */
@@ -26,5 +37,74 @@ router.get(
   statisticsController.getTotalPapersByAuthorId
 );
 
+/**
+ * @swagger
+ * /statistics/total-views-by-author/{author_id}:
+ *   get:
+ *     summary: Get total views of scientific papers by an author
+ *     tags: [Statistics]
+ *     parameters:
+ *       - in: path
+ *         name: author_id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the author
+ *     responses:
+ *       200:
+ *         description: Total views of scientific papers retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 author_id:
+ *                   type: string
+ *                   description: ID of the author
+ *                 total_views:
+ *                   type: number
+ *                   description: Total views of the author's scientific papers
+ *       500:
+ *         description: Internal server error
+ */
+router.get(
+  "/total-views-by-author/:author_id",
+  statisticsController.getTotalViewsByAuthorId
+);
+
+/**
+ * @swagger
+ * /statistics/total-downloads-by-author/{author_id}:
+ *   get:
+ *     summary: Get total downloads of scientific papers by an author
+ *     tags: [Statistics]
+ *     parameters:
+ *       - in: path
+ *         name: author_id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the author
+ *     responses:
+ *       200:
+ *         description: Total downloads of scientific papers retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 author_id:
+ *                   type: string
+ *                   description: ID of the author
+ *                 total_downloads:
+ *                   type: number
+ *                   description: Total downloads of the author's scientific papers
+ *       500:
+ *         description: Internal server error
+ */
+router.get(
+  "/total-downloads-by-author/:author_id",
+  statisticsController.getTotalDownloadsByAuthorId
+);
 
 module.exports = router;
